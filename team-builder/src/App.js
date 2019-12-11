d8b53b4b86a9069f9b12a535902d2d7bd65a63dc
Heber <hebergonza@gmail.com>
2019-12-10 22:45:08 -0500

    finished step 1

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [membersList, setMembersList] = useState([
+    {
+      name: "heber",
+      email: "@gmail",
+      role: "student"
+    }
+  ]);
+
+  console.log(membersList);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Team Members</h1>
+      <div>
+        {membersList.map((member) => {
+          return (
+            <div>
+              <h2>{member.name}</h2>
+              <h2>{member.email}</h2>
+              <h2>{member.role}</h2>
+            </div>
+          )
+          })}
+      </div>
     </div>
   );
 }
