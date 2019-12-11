abe282f4b165e50228d4d358d69de47be2d18439
Heber <hebergonza@gmail.com>
2019-12-11 00:56:53 -0500

    fixed members styling

--- team-builder/src/components/TeamMembers.js
+++ team-builder/src/components/TeamMembers.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const TeamMembers = (props) => {
   return (
-    <div className="member">
+    <div>
       {props.teamMembers.map(member => {
         return (
-          <div>
+          <div className="member">
             <h2>Name: {member.name}</h2>
             <h2>Email: {member.email}</h2>
             <h2>Role: {member.role}</h2>
